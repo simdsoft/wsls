4f4e3ede5e19165684f718bcdc2281d7ec421bc0
halx99 <halx99@live.com>
2018-05-31 19:13:40 +0800

    V2.0 Final done.

--- dists/ndk-wsls-2.0-final/install.bat
+++ dists/ndk-wsls-2.0-final/install.bat
@@ -3,14 +3,24 @@ pushd %~dp0
 
 set ndkRoot=%1
 if not defined ndkRoot set ndkRoot=%ANDROID_NDK_ROOT%
+
+if not defined ndkRoot set ndkRoot=D:\android_devenv\adt-bundle-windows\sdk\ndk-bundle
+
 if not defined ndkRoot echo Please specific ANDROID_NDK_ROOT! && goto :L_exit
 
+if not exist %ndkRoot% echo The directory not exist! && goto :L_exit
+
 call :InstPatch "%ndkRoot%\prebuilt\windows-x86_64\bin" make.exe
+
 call :InstPatch "%ndkRoot%\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin" arm-linux-androideabi-g++.exe
 call :InstPatch "%ndkRoot%\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin" arm-linux-androideabi-gcc.exe
-call :InstPatch "%ndkRoot%\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x" cc1plus.exe
+
 call :InstPatch "%ndkRoot%\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin" ld.exe
 call :InstPatch "%ndkRoot%\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin" as.exe
+call :InstPatch "%ndkRoot%\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin" ar.exe
+
+call :InstPatch "%ndkRoot%\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x" cc1plus.exe
+call :InstPatch "%ndkRoot%\toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x" collect2.exe
 
 goto :L_exit
 
