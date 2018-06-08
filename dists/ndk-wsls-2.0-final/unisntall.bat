9103a95c4f499a66c72c0c2dd0fee2b5a167cb02
halx99 <halx99@live.com>
2018-06-08 13:09:01 -0500

    Update unisntall.bat

--- dists/ndk-wsls-2.0-final/unisntall.bat
+++ dists/ndk-wsls-2.0-final/unisntall.bat
@@ -7,19 +7,19 @@ if not defined ndkRoot set ndkRoot=D:\android_devenv\adt-bundle-windows\sdk\ndk-
 
 pushd %ndkRoot%
 
-copy /y prebuilt\windows-x86_64\bin\ndk-make.exe prebuilt\windows-x86_64\bin\make.exe
-copy /y prebuilt\windows-x86_64\bin\ndk-cmp.exe prebuilt\windows-x86_64\bin\cmp.exe
+wsls-copy /y prebuilt\windows-x86_64\bin\ndk-make.exe prebuilt\windows-x86_64\bin\make.exe
+wsls-copy /y prebuilt\windows-x86_64\bin\ndk-cmp.exe prebuilt\windows-x86_64\bin\cmp.exe
 
 wsls-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin\ndk-arm-linux-androideabi-g++.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin\arm-linux-androideabi-g++.exe
-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin\ndk-arm-linux-androideabi-gcc.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin\arm-linux-androideabi-gcc.exe
+wsls-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin\ndk-arm-linux-androideabi-gcc.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\bin\arm-linux-androideabi-gcc.exe
 
-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\ndk-ld.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\ld.exe
-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\ndk-as.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\as.exe
-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\ndk-ar.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\ar.exe
+wsls-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\ndk-ld.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\ld.exe
+wsls-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\ndk-as.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\as.exe
+wsls-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\ndk-ar.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\arm-linux-androideabi\bin\ar.exe
 
-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\ndk-cc1.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\cc1.exe
-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\ndk-cc1plus.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\cc1plus.exe
-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\ndk-collect2.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\collect2.exe
+wsls-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\ndk-cc1.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\cc1.exe
+wsls-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\ndk-cc1plus.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\cc1plus.exe
+wsls-copy /y toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\ndk-collect2.exe toolchains\arm-linux-androideabi-4.9\prebuilt\windows-x86_64\libexec\gcc\arm-linux-androideabi\4.9.x\collect2.exe
 
 popd
 
